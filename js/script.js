ced26534146d7d974df7e93824ae393722784d48
lucas-nguyen0 <lucas.nguyen@mths.ca>
2025-06-02 13:14:19 -0400

    created files, added to index.html

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2025 Lucas Nguyen All rights reserved
+//
+// Created by: Lucas Nguyen
+// Created on: Apr 2025
+// This file contains the JS functions for index.html
+
+'use strict'
