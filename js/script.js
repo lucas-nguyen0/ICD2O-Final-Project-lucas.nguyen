a457b31561fdd7504d434639a621c0faa64ce4e4
lucas-nguyen0 <lucas.nguyen@mths.ca>
2025-06-04 17:30:29 +0000

    js and css changes

--- js/script.js
+++ js/script.js
@@ -6,3 +6,18 @@
 
 'use strict'
 
+const clickArea = document.getElementById('#click-area')
+const displayText = document.getElementById('#display-text')
+const scores = document.getElementById('#score')
+
+const scoreHistory = []
+
+const MIN_MS_UNTIL_CHANGE = 3000
+const MAX_MS_UNTIL_CHANGE = 7000
+
+let msSinceColorSwitch = 0
+let waitingForClick = false
+
+function play() {
+  const msUntilChange = Math.floor(Math.random() * (MAX_MS_UNTIL_CHANGE - MIN_MS_UNTIL_CHANGE)) + MIN_MS_UNTIL_CHANGE
+}
