dff47656eb4458abe2ec6d079796aec75cf46d59
lucas-nguyen0 <lucas.nguyen@mths.ca>
2025-06-09 16:13:36 +0000

    on click, screen green and waits for click

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@
 
 'use strict'
 
-const clickArea = document.getElementById('#click-area')
-const displayText = document.getElementById('#display-text')
-const scores = document.getElementById('#score')
+const clickArea = document.getElementById('click-area')
+const displayText = document.getElementById('display-text')
+const scores = document.getElementById('score')
 
 const scoreHistory = []
 
@@ -19,5 +19,41 @@ let msSinceColorSwitch = 0
 let waitingForClick = false
 
 function play() {
+  // Time until color switch
   const msUntilChange = Math.floor(Math.random() * (MAX_MS_UNTIL_CHANGE - MIN_MS_UNTIL_CHANGE)) + MIN_MS_UNTIL_CHANGE
+
+  // Revert color back to red
+  clickArea.style.backgroundColor = null;
+
+  // Clear screen
+  document.getElementById("display-text").innerHTML = ""
+
+  // When the color switches
+  setTimeout(() => {
+    msSinceColorSwitch = Date.now()
+
+    clickArea.style.backgroundColor = "#009578"
+    waitingForClick = true
+  }, msUntilChange);
+}
+
+// On Click
+clickArea.addEventListener("click", function () {
+  if (waitingForClick === true) {
+    // Find time
+    const score = Date.now() - msSinceColorSwitch
+
+    waitingForClick = false
+    document.getElementById("display-text").innerHTML = "Your time was " + score + "ms. Click again!"
+
+    addScore()
+  } else {
+    play()
+  }
+})
+
+function addScore(score) {
+  // Add scoe to beginning of array
+  scoreHistory.unshift(score)
+
 }
