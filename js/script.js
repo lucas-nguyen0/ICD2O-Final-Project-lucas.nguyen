e5e66c46c3c3afe0cb0d526c92b009cfa01efabd
lucas-nguyen0 <lucas.nguyen@mths.ca>
2025-06-12 17:28:51 +0000

    ran standard fix and fixed css

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // Copyright (c) 2025 Lucas Nguyen All rights reserved
 //
 // Created by: Lucas Nguyen
-// Created on: Apr 2025
+// Created on: Jun 2025
 // This file contains the JS functions for index.html
 
 'use strict'
 
 const clickArea = document.getElementById('click-area')
 const displayText = document.getElementById('display-text')
-let starImage = ''
+const starImage = ''
 
 const MIN_MS_UNTIL_CHANGE = 3000
 const MAX_MS_UNTIL_CHANGE = 7000
@@ -19,34 +19,13 @@ let waitingForPlay = true
 let colorSwitchTimeout = null
 let bestScore = Infinity
 
-// on click to start
-function play() {
-  // Time until color switch
-  const msUntilChange = Math.floor(Math.random() * (MAX_MS_UNTIL_CHANGE - MIN_MS_UNTIL_CHANGE)) + MIN_MS_UNTIL_CHANGE
-  waitingForPlay = false
-
-  // Revert color back to red
-  clickArea.style.backgroundColor = null;
-
-  // Clear screen
-  document.getElementById("display-text").innerHTML = "Wait for green..."
-
-  // When the color switches
-  colorSwitchTimeout = setTimeout(() => {
-    msSinceColorSwitch = Date.now()
-
-    clickArea.style.backgroundColor = "#009578"
-    document.getElementById("display-text").innerHTML = ""
-    waitingForClick = true
-  }, msUntilChange);
-}
-
 // On click
-clickArea.addEventListener("click", function () {
+clickArea.addEventListener('click', function () {
+  // early click
   if (!waitingForPlay && !waitingForClick) {
     clearTimeout(colorSwitchTimeout)
     waitingForPlay = true
-    document.getElementById("display-text").innerHTML = "You clicked too early! Click to try again."
+    document.getElementById('display-text').innerHTML = 'You clicked too early! Click to try again.'
   } else if (waitingForClick === true) {
     // Find time
     const score = Date.now() - msSinceColorSwitch
@@ -55,23 +34,45 @@ clickArea.addEventListener("click", function () {
     if (score < bestScore) {
       bestScore = score
     }
-    if (score >= 450) {
-      document.getElementById("display-text").innerHTML =
-        "Your time was " + score + "ms. Click again! <br/>Best Score: " + bestScore + "ms<br/><br/><img src='./images/1-star.png'>"
+    if (score >= 500) {
+      document.getElementById('display-text').innerHTML =
+        'Your time was ' + score + 'ms. Click again! <br/>Best Score: ' + bestScore + "ms<br/><br/><img src='./images/1-star.png'>"
     } else if (score > 400) {
-      document.getElementById("display-text").innerHTML =
-        "Your time was " + score + "ms. Click again! <br/>Best Score: " + bestScore + "ms<br/><br/><img src='./images/2-star.png'>"
-    } else if (score > 325) {
-      document.getElementById("display-text").innerHTML =
-        "Your time was " + score + "ms. Click again! <br/>Best Score: " + bestScore + "ms<br/><br/><img src='./images/3-star.png'>"
-    } else if (score > 290) {
-      document.getElementById("display-text").innerHTML =
-        "Your time was " + score + "ms. Click again! <br/>Best Score: " + bestScore + "ms<br/><br/><img src='./images/4-star.png'>"
+      document.getElementById('display-text').innerHTML =
+        'Your time was ' + score + 'ms. Click again! <br/>Best Score: ' + bestScore + "ms<br/><br/><img src='./images/2-star.png'>"
+    } else if (score > 350) {
+      document.getElementById('display-text').innerHTML =
+        'Your time was ' + score + 'ms. Click again! <br/>Best Score: ' + bestScore + "ms<br/><br/><img src='./images/3-star.png'>"
+    } else if (score > 300) {
+      document.getElementById('display-text').innerHTML =
+        'Your time was ' + score + 'ms. Click again! <br/>Best Score: ' + bestScore + "ms<br/><br/><img src='./images/4-star.png'>"
     } else {
-      document.getElementById("display-text").innerHTML =
-        "Your time was " + score + "ms. Click again! <br/>Best Score: " + bestScore + "ms<br/><br/><img src='./images/5-star.png'>"
+      document.getElementById('display-text').innerHTML =
+        'Your time was ' + score + 'ms. Click again! <br/>Best Score: ' + bestScore + "ms<br/><br/><img src='./images/5-star.png'>"
     }
   } else {
     play()
   }
 })
+
+// game start click
+function play () {
+  // Time until color switch
+  const msUntilChange = Math.floor(Math.random() * (MAX_MS_UNTIL_CHANGE - MIN_MS_UNTIL_CHANGE)) + MIN_MS_UNTIL_CHANGE
+  waitingForPlay = false
+
+  // Revert color back to red
+  clickArea.style.backgroundColor = null
+
+  // Clear screen
+  document.getElementById('display-text').innerHTML = 'Wait for green...'
+
+  // When the color switches
+  colorSwitchTimeout = setTimeout(() => {
+    msSinceColorSwitch = Date.now()
+
+    clickArea.style.backgroundColor = '#009578'
+    document.getElementById('display-text').innerHTML = ''
+    waitingForClick = true
+  }, msUntilChange)
+}
